4d03a97fa18542493b33c17be9d2ea925b71b863
aflayton <aflayton@gmail.com>
2019-04-11 17:30:45 -0400

    Update sysfile.c

--- sysfile.c
+++ sysfile.c
@@ -15,7 +15,7 @@
 #include "sleeplock.h"
 #include "file.h"
 #include "fcntl.h"
-extern int readcount = 0;
+int readcount = 0;
 
 // Fetch the nth word-sized system call argument as a file descriptor
 // and return both the descriptor and the corresponding struct file.
