dd12384ff396a18c31a27eb08175661523165f98
aflayton <aflayton@gmail.com>
2019-04-11 17:26:35 -0400

    Update sysfile.c

--- sysfile.c
+++ sysfile.c
@@ -15,6 +15,7 @@
 #include "sleeplock.h"
 #include "file.h"
 #include "fcntl.h"
+extern int readcount = 0;
 
 // Fetch the nth word-sized system call argument as a file descriptor
 // and return both the descriptor and the corresponding struct file.
@@ -72,10 +73,11 @@ sys_read(void)
   struct file *f;
   int n;
   char *p;
-  readcount++;
 
   if(argfd(0, 0, &f) < 0 || argint(2, &n) < 0 || argptr(1, &p, n) < 0)
     return -1;
+    
+  readcount++;
   return fileread(f, p, n);
 }
 
