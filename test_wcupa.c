1c73d3b00bbc688891eee00fdcf658784cbc06b7
aflayton <aflayton@gmail.com>
2019-04-11 17:19:29 -0400

    Update test_wcupa.c

--- test_wcupa.c
+++ test_wcupa.c
@@ -4,8 +4,6 @@
  
 int main(void) {
   printf(1, "West Chester University was founded in %d\n", wcupa());
-  printf(1, "readcount before: %d\n", getreadcount());
-  read();
-  printf(1, "readcount after: %d\n", getreadcount());
+  printf(1, "Current readcount: %d\n", getreadcount());
   exit();
 }
